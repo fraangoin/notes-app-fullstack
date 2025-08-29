262868b7f8fe950b071698140d7f08647ef607d3
fraan goin <fraangoin@gmail.com>
2025-08-28 23:53:56 -0300

    refactor: add validation with ParseIntPipe for ID parameters

--- backend/src/notes/notes.controller.ts
+++ backend/src/notes/notes.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
 import { NotesService } from './notes.service';
 import { CreateNoteDto } from './dto/create-note.dto';
 import { UpdateNoteDto } from './dto/update-note.dto';
@@ -28,27 +28,27 @@ export class NotesController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.notesService.findOne(+id);
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.notesService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateNoteDto: UpdateNoteDto) {
-    return this.notesService.update(+id, updateNoteDto);
+  update(@Param('id', ParseIntPipe) id: number, @Body() updateNoteDto: UpdateNoteDto) {
+    return this.notesService.update(id, updateNoteDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.notesService.remove(+id);
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.notesService.remove(id);
   }
 
   @Patch(':id/archive')
-  archive(@Param('id') id: string) {
-    return this.notesService.archive(+id);
+  archive(@Param('id', ParseIntPipe) id: number) {
+    return this.notesService.archive(id);
   }
 
   @Patch(':id/unarchive') 
-  unarchive(@Param('id') id: string) {
-    return this.notesService.unarchive(+id);
+  unarchive(@Param('id', ParseIntPipe) id: number) {
+    return this.notesService.unarchive(id);
   }
 }
